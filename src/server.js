2886b06209f92fd2e7a3acbcea5ce39462a501b1
Flovedev <flove.dev@gmail.com>
2023-03-06 14:32:17 +0000

    install cloud

--- src/server.js
+++ src/server.js
@@ -16,9 +16,27 @@ import {
 const server = Express();
 const port = 3001;
 const publicFolderPath = join(process.cwd(), "./public");
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
 
 server.use(Express.static(publicFolderPath));
-server.use(cors());
+server.use(
+  cors({
+    origin: (currentOrigin, corsNext) => {
+      if (!currentOrigin || whitelist.indexOf(currentOrigin) !== -1) {
+        // origin is in the whitelist
+        corsNext(null, true);
+      } else {
+        // origin is not in the whitelist
+        corsNext(
+          createHttpError(
+            400,
+            `Origin ${currentOrigin} is not in the whitelist!`
+          )
+        );
+      }
+    },
+  })
+);
 server.use(Express.json());
 
 server.use("/authors", authorsRouter);
