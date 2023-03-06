4d77a6b8c4f6e9ff4c75a094a71951eeb9fd75a8
Flovedev <flove.dev@gmail.com>
2023-03-06 16:52:37 +0000

    updates

--- src/api/authorsFiles/index.js
+++ src/api/authorsFiles/index.js
@@ -1,26 +1,36 @@
 import Express from "express";
 import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import { extname } from "path";
 import { saveAuthorsAvatars, getAuthors } from "../../lib/fs-tools.js";
+import { v2 as cloudinary } from "cloudinary";
 
 const authorsFilesRouter = Express.Router();
+const cloudinaryUploader = multer({
+  storage: new CloudinaryStorage({
+    cloudinary,
+    params: { folder: "blogPosts/avatar" },
+  }),
+}).single("avatar");
 
 authorsFilesRouter.post(
   "/:id/uploadAvatar",
   multer().single("avatar"),
+  cloudinaryUploader,
   async (req, res, next) => {
     try {
-      const authorsArray = await getAuthors();
-      const author = authorsArray.find((e) => e.ID === req.params.id);
-      if (author) {
-        const originalFileExtension = extname(req.file.originalname);
-        const fileName = req.params.id + originalFileExtension;
-        await saveAuthorsAvatars(fileName, req.file.buffer);
+      console.log("working");
+      // const authorsArray = await getAuthors();
+      // const author = authorsArray.find((e) => e.ID === req.params.id);
+      // if (author) {
+      //   const originalFileExtension = extname(req.file.originalname);
+      //   const fileName = req.params.id + originalFileExtension;
+      //   await saveAuthorsAvatars(fileName, req.file.buffer);
 
-        res.send({ message: "file uploaded" });
-      } else {
-        res.status(404).send(`Author with the id: ${req.params.id} not found.`);
-      }
+      //   res.send({ message: "file uploaded" });
+      // } else {
+      //   res.status(404).send(`Author with the id: ${req.params.id} not found.`);
+      // }
     } catch (error) {
       next(error);
     }
